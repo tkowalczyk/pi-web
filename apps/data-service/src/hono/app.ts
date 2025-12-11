25d78e0013535d463f5398edc7408116775a2026
Tomasz Kowalczyk <tomek@kownet.info>
2025-12-11 21:50:12 +0100

    data-service on stage env

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -3,10 +3,12 @@ import { Hono } from "hono";
 
 export const app = new Hono<{ Bindings: Env }>();
 
-app.get("/worker", (c) =>
+app.get("/worker/:id", (c) =>
   c.json({
     name: "powiadomienia-info Worker",
     version: "0.0.1",
     description: "Simple notification service on top of Cloudflare Workers",
+    id: c.req.param("id"),
+    var: env.MY_VAR
   }),
 );
